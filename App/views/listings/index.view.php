3205e56141215ad5e0df22baf49c0bcb0ed8c22e
Tre'von Kitchen <trekitch10@gmail.com>
2025-04-23 02:43:20 -0400

    Final changes for project

--- App/views/listings/index.view.php
+++ App/views/listings/index.view.php
@@ -7,7 +7,13 @@ loadPartial('top-banner');
 <!-- Job Listings -->
 <section>
     <div class="container mx-auto p-4 mt-4">
-        <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">All Jobs</div>
+        <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">
+            <?php if ($keywords): ?>
+                Search Results for: <?= htmlspecialchars($keywords) ?>
+            <?php else: ?>
+                All Jobs
+            <?php endif; ?>
+        </div>
         <?php loadPartial('message') ?>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
             <!-- Job Listing 1: Software Engineer -->
